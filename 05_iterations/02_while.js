1a06e3b98e57a50d447346fb55df1ab56e6173a5
Shubh <120670270+Shubham02022002@users.noreply.github.com>
2024-05-01 17:16:16 +0000

    while

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,25 @@
+let index = 0;
+
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}.`);
+//   index = index + 2;
+// }
+
+// all the loops requires some initialization, condition and updation values to terminate. 
+
+// let myArray=["Ironman","Thor","Hulk"];
+// let i=0;
+// while(i<myArray.length){
+//     console.log(`My superhero is ${myArray[i]}.`);
+//     i++;
+// }
+
+let score=11;
+
+do {
+    console.log(`Current score is ${score}.`);
+    score++;
+} while (score<=10);
+
+// console.log(score); --> 12 
+// do while loop is executed at least one time. 
