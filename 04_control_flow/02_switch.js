0ad9d50359cafbcf29357551afde3547d179482d
Shubh <120670270+Shubham02022002@users.noreply.github.com>
2024-05-01 02:40:07 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 2;
+const birthdayMonth="month"
+switch (birthdayMonth) {
+  case "month":
+    console.log("yo");
+    break;
+  case 2:
+    console.log("February");
+    // break;
+  case 3:
+    console.log("March");
+    break;
+  default:
+    console.log("default");
+}
+// if break is not used then it will execute whole code till the end except default. 
+
+// in key we can check for numbers and strings as well. 
